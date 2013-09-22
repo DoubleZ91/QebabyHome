4f7cecf4417cb2d16827a6812bad9dcf5cb8aa1b
DoubleZ <zhouzhanpeng@youmi.net>
2013-09-22 18:09:56 +0800

    add register module

---
+++ BabyRegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  BabyRegisterViewController.h
+//  QeBaby
+//
+//  Created by DoubleZ on 13-9-22.
+//  Copyright (c) 2013年 DoubleZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BabyRegisterViewController : UIViewController
+
+@end
---
+++ BabyRegisterViewController.m
@@ -0,0 +1,38 @@
+//
+//  BabyRegisterViewController.m
+//  QeBaby
+//
+//  Created by DoubleZ on 13-9-22.
+//  Copyright (c) 2013年 DoubleZ. All rights reserved.
+//
+
+#import "BabyRegisterViewController.h"
+
+@interface BabyRegisterViewController ()
+
+@end
+
+@implementation BabyRegisterViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
